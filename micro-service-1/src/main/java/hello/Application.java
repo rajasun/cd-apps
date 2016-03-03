94290808af34af85f1b82ac0b7381c8307221525
rajasun <rajasun34@gmail.com>
2016-03-03 13:22:19 +0530

    Trigger a build

--- micro-service-1/src/main/java/hello/Application.java
+++ micro-service-1/src/main/java/hello/Application.java
@@ -12,8 +12,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @RestController
 public class Application {
-
-	
     @Value("${db.pool.size}")
     String poolSize;
     
@@ -52,18 +50,10 @@ public class Application {
 	}
 
 	
-	String invokeUtilityService(String url)
-	{
-	   
-	     
+	String invokeUtilityService(String url)	{
 	    RestTemplate restTemplate = new RestTemplate();
 	    String result = restTemplate.getForObject(url, String.class);
-	     
 	    System.out.println(result);
-	    
 	    return result;
-		
 	}
-	
-	
 }
